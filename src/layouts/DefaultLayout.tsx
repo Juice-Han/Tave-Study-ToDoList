beb33408379c1b2b835e2879961d016563ebc299
juhanPark <iiiamst16@gmail.com>
2025-10-03 23:24:00 +0900

    clg 삭제

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -6,7 +6,6 @@ const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
     const handleResize = () => {
       const vh = window.innerHeight * 0.01;
       document.documentElement.style.setProperty("--vh", `${vh}px`);
-      console.log('resize')
     };
     window.addEventListener("resize", handleResize);
     return () => {
